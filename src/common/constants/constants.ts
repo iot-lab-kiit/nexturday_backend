e5be4e6b14da91ef3bdeaaafd7fe1d0e9f1659e1
solo8116 <gr3y.ma7t3r@gmail.com>
2025-01-19 23:17:27 +0530

    limit 1 mb image upload

--- src/common/constants/constants.ts
+++ src/common/constants/constants.ts
@@ -3,3 +3,4 @@ export const TOTAL_IMAGES = 5;
 export const CHUNK_SIZE = 5 * 1024 * 1024;
 export const CROUSEL = 5;
 export const TOKEN_EXPIRY = '7d';
+export const FILE_SIZE = 1 * 1024 * 1024;
--- src/middlewares/uploader/uploader.middleware.ts
+++ src/middlewares/uploader/uploader.middleware.ts
@@ -1,10 +1,11 @@
 import multer from 'multer';
+import { FILE_SIZE } from '../../common/constants';
 
 export class UploaderMiddleware {
   public uploader: multer.Multer;
 
   constructor() {
     const storage = multer.memoryStorage();
-    this.uploader = multer({ storage });
+    this.uploader = multer({ storage, limits: { fileSize: FILE_SIZE } });
   }
 }
