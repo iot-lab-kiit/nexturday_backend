f9f59d22f67c2f07829b91c4d391aea1820f934f
solo8116 <gr3y.m47t3r@gmail.com>
2025-02-20 21:40:42 +0530

    fix: event already joined error

--- src/services/event/participant/team.service.ts
+++ src/services/event/participant/team.service.ts
@@ -206,6 +206,7 @@ export class TeamService {
     }
     const registered = await this.prisma.team.findFirst({
       where: {
+        eventId,
         OR: [
           {
             leaderId: participantId,
